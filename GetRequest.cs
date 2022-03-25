c3983027886581566c229f44a13df54dcb908a34
majorjk228 <sana-ivanov1@yandex.ru>
2022-03-25 19:17:23 +0300

    Добавьте файлы проекта.

---
+++ GetRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ParserApi
+{
+    public class GetRequest
+    {
+        HttpWebRequest _request; // создаем объект
+        string _adress; //заводим переменная
+
+        public string Response { get; set; }
+
+        public GetRequest(string adress) //конструктор который принимает адресс
+        {
+            _adress = adress;
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_adress); //создаем сам запрос
+            _request.Method = "Get";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse(); //ответ сервера
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd(); //записываем ответ
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using ParserApi;
+using System;
+
+namespace Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var key = "2"; можно использовать как "https://reqres.in/api/users?page={key}" класть значение из переменной
+
+            //var users = "users"; 
+
+            var request = new GetRequest($"https://reqres.in/api/users?page=1"); //образаемся к апи
+
+            request.Run();
+
+            var response = request.Response; // ответ от портала
+
+            var json = JObject.Parse(response);//выводим из джсон
+
+            var data = json["data"];
+
+            foreach (var elements in data)
+            {
+                var name = elements["first_name"];
+                var email = elements["email"];
+                var lastname = elements["last_name"];
+
+                Console.WriteLine($"Имя:{name} \n Фамилия:{lastname} \n Мыло:{email}"); //вывожу данные из джсона на консоль(изи парс):)
+            }
+        }
+    }
+}
